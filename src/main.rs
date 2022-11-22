f29f60688cdcecfee51eae26d11c9121487416ce
thordal-indykite <simon.thordal@indykite.com>
2022-11-22 22:08:22 +0100

    test: add tests and comment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+/// Gets a random coordinate
+/// 
+/// # Examples 
+/// 
+/// ```
+/// let n = 10;
+/// let coord = conway::get_coordinates(n);
+/// assert!(coord[0] < n)
+/// assert!(coord[1] < n)
+/// ``` 
+pub fn get_coordinates(n: u32) -> (u32, u32) {
+    (1,1)
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_coordinates() {
+        assert_eq!(get_coordinates(10), (1,1));
+    }
 }
